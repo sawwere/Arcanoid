b49ded7c04f13ef5d8ea893af87973a520552dca
OlegTerV <www.oleg2002ter@mail.ru>
2024-10-19 20:21:32 +0300

    task a1

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -9,18 +9,22 @@ public class BlockScript : MonoBehaviour
     public GameObject textObject;
     private TMP_Text textComponent;
     private PlayerScript playerScript;
-
-    public int hitsToDestroy;
+	private GameObject BonusObject;
+	private GameObject bonus;
+	public GameDataScript gameData;
+	public int hitsToDestroy;
     public int points;
+
     void Start()
     {
         if (textObject != null)
         {
             textComponent = textObject.GetComponent<TMP_Text>();
             textComponent.text = hitsToDestroy.ToString();
-        }
+		}
         playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
-    }
+		BonusObject = GameObject.Find("Bonus");
+	}
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -30,7 +34,15 @@ public class BlockScript : MonoBehaviour
             {
                 Destroy(gameObject);
                 playerScript.BlockDestroyed(points);
-            }
+
+				if (gameObject.name== "Green Block Variant(Clone)")
+				{
+					bonus = Instantiate(BonusObject, gameObject.transform.position, gameObject.transform.rotation);
+					bonus.AddComponent<BonusBase>();
+                    bonus.GetComponent<BonusBase>().build(gameData, "+100");
+				}
+
+			}
                 
             else if (textComponent != null)
                 textComponent.text = hitsToDestroy.ToString();
---
+++ Assets/Scripts/BonusBase.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class BonusBase : MonoBehaviour
+{
+	public Color[] colors = { Color.yellow, Color.green, Color.red, Color.blue, Color.white, Color.black };
+	public TMP_Text textComponent;
+	private Rigidbody2D rb;
+	public GameDataScript gameData;
+
+	void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector2(0f, -0.5f);
+	}
+
+	void Update()
+    {
+        
+    }
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		BonusActivate(100);
+		Destroy(gameObject);
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy(gameObject);
+	}
+
+	void BonusActivate(int points)
+	{
+		gameData.points += points;
+	}
+
+	public void build(GameDataScript gameData, string text)
+	{
+		textComponent = transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>();
+		this.gameData = gameData;
+		switch (text)
+		{
+			case "+100":
+				gameObject.GetComponent<SpriteRenderer>().color = colors[0];
+				textComponent.text = text;
+				textComponent.color = colors[5];
+				break;
+			case "+100":
+				gameObject.GetComponent<SpriteRenderer>().color = colors[0];
+				textComponent.text = text;
+				textComponent.color = colors[5];
+				break;
+			case "+100":
+				gameObject.GetComponent<SpriteRenderer>().color = colors[0];
+				textComponent.text = text;
+				textComponent.color = colors[5];
+				break;
+			case "+100":
+				gameObject.GetComponent<SpriteRenderer>().color = colors[0];
+				textComponent.text = text;
+				textComponent.color = colors[5];
+				break;
+		}
+	}
+}
